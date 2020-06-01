3fbbeae6365e117fa1fbbf6c7c4e9674adb0444e
faiwudevse <faidevse@gamil.com>
2020-06-01 15:09:05 -0400

    change

--- Week_06/0091.DecodeWays.java
+++ Week_06/0091.DecodeWays.java
@@ -7,7 +7,7 @@ class Solution {
 
         int[] dp = new int[n +1];
 
-        dp[0] = '1';
+        dp[0] = 1;
 
         dp[1] = s.charAt(0) != '0' ? 1 :0;
 
@@ -16,11 +16,11 @@ class Solution {
             int twoDigits = Integer.valueOf(s.substring(i - 2, i));
 
             if (oneDigit >= 1 && oneDigit <= 9) {
-                dp[i] = dp[i - 1];
+                dp[i] += dp[i - 1];
             }
 
             if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i] = dp[i - 2];
+                dp[i] += dp[i - 2];
             }
         }
 
